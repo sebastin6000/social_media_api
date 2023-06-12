e343fdfa5d831fe14de9bbaee1be464105eec03f
Sebastin Raj <134362610+sebastin6000@users.noreply.github.com>
2023-06-12 13:34:24 +0530

    mongo db database connection setup finished

--- server/index.js
+++ server/index.js
@@ -34,3 +34,15 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage });
+
+// MONGO-DB DATABASE CONNECTION CONFIGURATION //
+const PORT = process.env.PORT || 6001;
+mongoose
+  .connect(process.env.MONGO_CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connected to database`));
