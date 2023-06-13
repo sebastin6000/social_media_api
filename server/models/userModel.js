bfda79d46d52674f4f3a0aa2e4f258b04b5b367b
Sebastin Raj <134362610+sebastin6000@users.noreply.github.com>
2023-06-13 12:32:51 +0530

    completed userschema and created a user model

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,11 +1,50 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    // the first name input field has these below properties
-    type: String,
-    require: true,
-    min: 2,
-    max: 50,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      // the first name input field has these below properties
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    lastName: {
+      // the last name input field has these below properties
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    email: {
+      // the email input field has these below properties
+      type: String,
+      required: true,
+      max: 50,
+      unique: true,
+    },
+    password: {
+      // the password input field has these below properties
+      type: String,
+      required: true,
+      min: 5,
+    },
+    picturePath: {
+      type: String,
+      default: "",
+    },
+    friends: {
+      type: Array,
+      default: [],
+    },
+    location: String,
+    occupation: String,
+    viewedProfile: Number,
+    impression: Number,
   },
-});
+  { timestamps: true }
+);
+
+// the below code is for creating a new user model
+// with the user model we create new user documents
+const userDocuments = new mongoose.model("User", userSchema);
